d0f12271f1650992f772f8e0e4c98ccd3857cab3
Peer Jüttner <peer.juettner@isolutions.ch>
2019-06-20 20:05:40 +0200

    Add buttons to cards for status switching

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,6 +6,10 @@ interface CardProps {
   id: number;
   handleCardStatusChange: (id: number, value: Status) => void;
 }
+enum ButtonType {
+  left,
+  right
+}
 
 export class Card extends Component<CardProps, {}> {
   constructor(props: CardProps) {
@@ -18,11 +22,18 @@ export class Card extends Component<CardProps, {}> {
           <label className="card-label">
             {this.props.title}
             <br />
+            {this.props.status !== Status.Todo && (
+              <button onClick={e => this.handleButtonClick(e, ButtonType.left)}>{"<--"}</button>
+            )}
+
             <select onChange={this.handleCardStatusChange} value={this.props.status}>
               <option value={Status.Todo}>Todo</option>
               <option value={Status.Doing}>Doing</option>
               <option value={Status.Done}>Done</option>
             </select>
+            {this.props.status !== Status.Done && (
+              <button onClick={e => this.handleButtonClick(e, ButtonType.right)}>{"-->"}</button>
+            )}
           </label>
         </form>
       </div>
@@ -31,6 +42,15 @@ export class Card extends Component<CardProps, {}> {
   handleCardStatusChange = (event: ChangeEvent<HTMLSelectElement>) => {
     console.log(event.target.nodeValue);
     this.props.handleCardStatusChange(this.props.id, parseInt(event.currentTarget.value));
+  };
+
+  handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>, type: ButtonType) => {
+    e.preventDefault();
+    if (type === ButtonType.left) {
+      this.props.handleCardStatusChange(this.props.id, this.props.status - 1);
+    } else {
+      this.props.handleCardStatusChange(this.props.id, this.props.status + 1);
+    }
   };
 }
 
